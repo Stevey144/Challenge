317a3fa82cf06c194bfcdb3d7241fa6427ae968e
stevy <45201174+Stevey144@users.noreply.github.com>
2024-05-30 11:39:00 +0100

    debugged and updated

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const db = require("./config/db");
+//const db = require("./config/db");
 const cors = require("cors");
-db();
+//db();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
